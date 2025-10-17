f7e258c6c1add65f7ef2e8d4eef336bbfc987539
Kellen <kellen.gary@gmail.com>
2025-10-17 18:18:51 -0400

    data store

---
+++ data_store.py
@@ -0,0 +1,24 @@
+
+class DataStore:
+    def __init__(self) -> None:
+        # create tables
+        pass
+
+    def write_packet(self) -> None:
+        # write data
+        pass
+
+    def write_action(self) -> None:
+        # write action data
+        pass
+
+    def read_packets(self) -> None:
+        # pull packets, 100 packets?
+        pass
+
+    def read_actions(self) -> None:
+        # pull all actions from action table
+        pass
+
+    def write_dummy_data(self) -> None:
+        pass
